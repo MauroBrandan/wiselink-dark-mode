96c3285adb6c5bfe4da7ce53c72c464cc8f210cc
Mauro Brandan <maurobrandan.mb@gmail.com>
2023-04-07 14:29:41 -0300

    Add font family

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { font } from '@/utils/font'
 
 export default function Home() {
 	return (
@@ -9,7 +10,8 @@ export default function Home() {
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<main>
+
+			<main className={font.className}>
 				<h1>Hello World!</h1>
 			</main>
 		</>
---
+++ src/utils/font.js
@@ -0,0 +1,5 @@
+import { Work_Sans } from 'next/font/google'
+
+export const font = Work_Sans({
+	subsets: ['latin'],
+})
